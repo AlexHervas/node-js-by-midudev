5ba6b2a6b2b0bb7577376a5e735574d45475743c
Alejandro Hervás <alesepto15@gmail.com>
2024-12-10 15:30:54 +0100

    Explaining the concept of globalThis.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// globalThis es la variable global por excelencia,todo lo que es GLOBAL sale de ella,
+// en el caso de Node la variable window no existe, se usa global
+// pero exite globalThis y es mejor usar esta última
+
 console.log(globalThis);
+
+// hace lo mismo ya que console.log sale de globalThis
+globalThis.console.log(globalThis);
