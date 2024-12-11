955db41307dd301ba0fe791ef25de04ff32b6924
Alejandro Hervás <alesepto15@gmail.com>
2024-12-11 10:30:06 +0100

    Using file system readFile, using callbacks.

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,14 +1,19 @@
 const fs = require('node:fs');
 
 console.log('Leyendo el primer archivo');
-// con readFileSync node no sabe cuando ha terminado de leer el archivo
-// y el proceso no continua hasta ser leido. Necesitamos usar readFile ya que es asíncrono
-// y deja que el hilo de ejecución siga mientras va leyengo el archivo
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
+/* 
+   con readFileSync node no sabe cuando ha terminado de leer el archivo
+   y el proceso no continua hasta ser leido. Necesitamos usar readFile ya que es asíncrono
+   y deja que el hilo de ejecución siga mientras va leyengo el archivo
+*/
+fs.readFile('./archivo.txt', 'utf-8', (error, text) => { // ejecuta el callback cuando termina
+    console.log(text);
+})
 
-console.log(text);
 console.log('Hacer cosas mientras se lee el archivo...');
 
 console.log('Leyendo el segundo archivo');
 
-const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+fs.readFile('./archivo2.txt', 'utf-8', (error, text) => {
+    console.log(text);
+})
