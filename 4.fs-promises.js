cde0a6d1f5e9322b61234900a8faa966567c062b
Alejandro Hervás <alesepto15@gmail.com>
2024-12-11 10:36:06 +0100

    using node:fs/promises to use promises instead of callbacks.

---
+++ 4.fs-promises.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs/promises');
+
+console.log('Leyendo el primer archivo');
+/* 
+   con readFileSync node no sabe cuando ha terminado de leer el archivo
+   y el proceso no continua hasta ser leido. Necesitamos usar readFile ya que es asíncrono
+   y deja que el hilo de ejecución siga mientras va leyengo el archivo
+*/
+fs.readFile('./archivo.txt', 'utf-8')
+    .then(text => {
+        console.log(text);
+    })
+
+console.log('Hacer cosas mientras se lee el archivo...');
+
+console.log('Leyendo el segundo archivo');
+
+fs.readFile('./archivo2.txt', 'utf-8', )
+    .then(text => {
+        console.log(text);
+    })
