39c3e293c4bcfc01f2985393581a4a7315aa0738
Alejandro Hervás <alesepto15@gmail.com>
2024-12-11 21:37:20 +0100

    Transforming callback to promises, using promisify.

--- 4.fs-promises.js
+++ 4.fs-promises.js
@@ -1,10 +1,18 @@
+/* 
+    Esto sólo en los módulos nativos
+    que no tienen promesas nativas
+
+    const { promisify } = require('node:util')
+    const readFilePromise = promisify(fs.readFile);
+*/
+
 const fs = require('node:fs/promises');
 
 console.log('Leyendo el primer archivo');
 /* 
-   con readFileSync node no sabe cuando ha terminado de leer el archivo
-   y el proceso no continua hasta ser leido. Necesitamos usar readFile ya que es asíncrono
-   y deja que el hilo de ejecución siga mientras va leyengo el archivo
+    con readFileSync node no sabe cuando ha terminado de leer el archivo
+    y el proceso no continua hasta ser leido. Necesitamos usar readFile ya que es asíncrono
+    y deja que el hilo de ejecución siga mientras va leyengo el archivo
 */
 fs.readFile('./archivo.txt', 'utf-8')
     .then(text => {
